b812249bf5c52345fc6b71dc70bacbc2e949de8d
Infinical <inficalvin@gmail.com>
2019-04-13 20:57:06 +0300

    create knowldege base page

---
+++ lib/pages/knowldgebase.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Knowledge Base',
+      home: new Home(),
+      theme: new ThemeData(primaryColor: Colors.pink),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => new _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    var problems = ["Puberty","FGM","Early Marriage","Early Pregnancy",];
+    var myGridView = new GridView.builder(
+      itemCount: problems.length,
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemBuilder: (BuildContext context, int index) {
+        return new GestureDetector(
+          child: new Card(
+            elevation: 5.0,
+            child: new Container(
+              alignment: Alignment.center,
+              margin: new EdgeInsets.only(top: 10.0, bottom: 10.0,left: 10.0,right: 10.0),
+              child: new Text(problems[index]),
+            ),
+          ),
+          // onTap: () {
+          //   showDialog(
+          //       barrierDismissible: false,
+          //       context: context,
+          //       child: new CupertinoAlertDialog(
+          //         title: new Column(
+          //           children: <Widget>[
+          //             new Text("GridView"),
+          //             new Icon(
+          //               Icons.favorite,
+          //               color: Colors.red,
+          //             ),
+          //           ],
+          //         ),
+          //         content: new Text( spacecrafts[index]),
+          //         actions: <Widget>[
+          //           new FlatButton(
+          //               onPressed: () {
+          //                 Navigator.of(context).pop();
+          //               },
+          //               child: new Text("OK"))
+          //         ],
+          //       ));
+          // },
+        );
+      },
+    );
+
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Flutter GridView")
+      ),
+      body: myGridView,
+    );
+  }
+}
+void main() {
+  runApp(new MyApp());
+}
